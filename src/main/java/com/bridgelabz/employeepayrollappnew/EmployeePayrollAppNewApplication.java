abfab16320ae2f2a3d77ff1d281d3f1f09f48c51
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-11-07 17:47:34 +0530

    Employee payroll service app initialized.

---
+++ src/main/java/com/bridgelabz/employeepayrollappnew/EmployeePayrollAppNewApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollappnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeePayrollAppNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bridgelabz/employeepayrollappnew/EmployeePayrollAppNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollappnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollAppNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
