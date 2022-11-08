42b4bf7d3e26be64c9abf79c9aead352432f4d02
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-11-08 15:07:35 +0530

    Section 3 Use Lombok lib. Slf4j for logging and removing the unnecessary logging from spring framework in Employee Payroll App UC2

--- src/main/java/com/bridgelabz/employeepayrollappnew/EmployeePayrollAppNewApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollappnew/EmployeePayrollAppNewApplication.java
@@ -1,13 +1,18 @@
 package com.bridgelabz.employeepayrollappnew;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+// Use lombak Lib for Logging @Slf4j
+@Slf4j
 public class EmployeePayrollAppNewApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeePayrollAppNewApplication.class, args);
+        log.info("Employee Payroll App Started");
     }
 
 }
