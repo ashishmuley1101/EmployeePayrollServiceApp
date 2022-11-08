775c6da87402e680430144bcdd968f8c806ba86d
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-11-08 22:21:26 +0530

    Section 4 data validation and exception handler by in Employee Payroll App UC1

--- src/main/java/com/bridgelabz/employeepayrollappnew/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollappnew/dto/EmployeePayrollDTO.java
@@ -1,7 +1,17 @@
 package com.bridgelabz.employeepayrollappnew.dto;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+
 public class EmployeePayrollDTO {
+
+    // Validation from user input for name by regex
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message = "Employee name Invalid")
     public String name;
+
+    // Validation from user input for salary must be greater then 500
+    @Min(value = 500,message = "Minimum wages should bemore that 500")
     public long salary;
 
     public EmployeePayrollDTO(String name,long salary){
